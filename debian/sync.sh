a6391d3176cf299bee2fe14002de849fe2ac7333
Andreas Beckmann <anbe@debian.org>
2023-04-17 18:23:16 +0200

    Import Debian changes 525.105.17-1
    nvidia-open-gpu-kernel-modules (525.105.17-1) unstable; urgency=medium
    .
      * New upstream production branch release 525.105.17 (2023-03-30).
        * Fixed CVE-2023-0184, CVE-2023-0189, CVE-2023-0180, CVE-2023-0183,
          CVE-2023-0185, CVE-2023-0187, CVE-2023-0198, CVE-2023-0199,
          CVE-2023-0188, CVE-2023-0190, CVE-2023-0194, CVE-2023-0195,
          CVE-2023-0191.  (Closes: #1033783)
          https://nvidia.custhelp.com/app/answers/detail/a_id/5452
      * Refresh patches.
    .
    nvidia-open-gpu-kernel-modules (525.89.02-1) unstable; urgency=medium
    .
      * New upstream production branch release 525.89.02 (2023-02-08).
    .
    nvidia-open-gpu-kernel-modules (525.85.12-1) unstable; urgency=medium
    .
      * New upstream production branch release 525.85.12 (2023-01-30).
      * New upstream production branch release 525.85.05 (2023-01-19).
      * New upstream production branch release 525.78.01 (2023-01-05).
      * New upstream production branch release 525.60.13 (2022-12-05).
      * New upstream production branch release 525.60.11 (2022-11-28).
        * Fixed CVE-2022-34670, CVE-2022-34673, CVE-2022-34674, CVE-2022-34675,
          CVE-2022-34677, CVE-2022-34679, CVE-2022-34680, CVE-2022-34682,
          CVE-2022-34684, CVE-2022-42254, CVE-2022-42255, CVE-2022-42256,
          CVE-2022-42257, CVE-2022-42258, CVE-2022-42259, CVE-2022-42263,
          CVE-2022-42264, CVE-2022-42265.  (Closes: #1025279)
          https://nvidia.custhelp.com/app/answers/detail/a_id/5415
      * New upstream beta 525.53 (2022-11-10).
      * Refresh patches.
      * Update Lintian overrides.
    .
    nvidia-open-gpu-kernel-modules (520.56.06-1) unstable; urgency=medium
    .
      * New upstream new feature branch release 520.56.06 (2022-10-12).
      * Make the package build reproducibly.
      * Upload to unstable.
    .
    nvidia-open-gpu-kernel-modules (515.86.01-1) unstable; urgency=medium
    .
      * New upstream production branch release 515.86.01 (2022-11-22).
        * Fixed CVE-2022-34670, CVE-2022-34673, CVE-2022-34674, CVE-2022-34675,
          CVE-2022-34677, CVE-2022-34679, CVE-2022-34680, CVE-2022-34682,
          CVE-2022-34684, CVE-2022-42254, CVE-2022-42255, CVE-2022-42256,
          CVE-2022-42257, CVE-2022-42258, CVE-2022-42259, CVE-2022-42263,
          CVE-2022-42264, CVE-2022-42265.  (Closes: #1025279)
          https://nvidia.custhelp.com/app/answers/detail/a_id/5415
      * Refresh patches.
      * Upload to unstable.
    .
    nvidia-open-gpu-kernel-modules (515.76-1) experimental; urgency=medium
    .
      * New upstream production branch release 515.76 (2022-09-20).
      * Refresh patches.
    .
    nvidia-open-gpu-kernel-modules (515.65.01-1) experimental; urgency=medium
    .
      * New upstream production branch release 515.65.01 (2022-08-02).
      * New upstream production branch release 515.57 (2022-06-28).
      * Add versioned Provides: nvidia-open-kernel-dkms-any.
    .
    nvidia-open-gpu-kernel-modules (515.48.07-1) experimental; urgency=medium
    .
      * New upstream production branch release 515.48.07 (2022-05-31).
      * Refresh patches.
      * Add salsa-ci.yml.
    .
    nvidia-open-gpu-kernel-modules (515.43.04-2) experimental; urgency=medium
    .
      * Backport drm_connector_has_override_edid changes from 525.78.01 to fix
        kernel module build for Linux 6.2.
      * Disable stackprotector.
      * Bump Standards-Version to 4.6.2. No changes needed.
    .
    nvidia-open-gpu-kernel-modules (515.43.04-1) experimental; urgency=medium
    .
      * New upstream beta 515.43.04 (2022-05-11).
      * Upload to experimental.

---
+++ debian/sync.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+ngd=${1:-../../nvidia-graphics-drivers/nvidia-graphics-drivers}
+
+ignore="
+debian/changelog
+debian/control
+debian/control.models
+debian/copyright
+debian/gbp.conf
+debian/nvidia-kernel-dkms.docs
+debian/nvidia-kernel-source.docs
+debian/nvidia-kernel-support.links.in
+debian/rules
+debian/rules.defs
+debian/sync.sh
+debian/watch
+debian/source/lintian-overrides
+debian/tests/control
+debian/upstream/metadata
+"
+
+for f in $(find debian -type f)
+do
+	for i in $ignore
+	do
+		if [ "$f" = "$i" ]; then
+			continue 2
+		fi
+	done
+
+	if [ -f "$ngd/$f" ]; then
+		if ! cmp -s "$ngd/$f" "$f" ; then
+			cp -v "$ngd/$f" "$f"
+		fi
+	fi
+done
